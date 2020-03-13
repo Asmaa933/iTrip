a3da1ef025a4da7011cc9bcb1d13f5dd50838bd5
ramzix02 <51852034+ramzix02@users.noreply.github.com>
2020-03-13 22:26:05 +0200

    add intro slider..

---
+++ app/src/main/java/com/andro/itrip/AppIntroActivity.java
@@ -0,0 +1,44 @@
+package com.andro.itrip;
+
+import android.content.Intent;
+import android.os.Bundle;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
+import com.andro.itrip.R;
+import com.andro.itrip.loginActivity.LoginActivity;
+import com.andro.itrip.mainActivity.MainActivity;
+import com.github.paolorotolo.appintro.AppIntro;
+import com.github.paolorotolo.appintro.AppIntroFragment;
+
+public class AppIntroActivity extends AppIntro {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setContentView(R.layout.activity_app_intro);
+
+        addSlide(AppIntroFragment.newInstance("Plan your trips.", "Daily planner, to manage your trips.",
+                R.drawable.first, ContextCompat.getColor(getApplicationContext(), R.color.colorPrimary)));
+        addSlide(AppIntroFragment.newInstance("Get Notification", "Suit up. It's time for fun.",
+                R.drawable.second, ContextCompat.getColor(getApplicationContext(), R.color.colorPrimary)));
+        addSlide(AppIntroFragment.newInstance("Start your trip.", "Step by step, till your destination!",
+                R.drawable.third, ContextCompat.getColor(getApplicationContext(), R.color.colorPrimary)));
+    }
+
+
+    @Override
+    public void onDonePressed(Fragment currentFragment) {
+        super.onDonePressed(currentFragment);
+        Intent intent=new Intent(getApplicationContext(), LoginActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    @Override
+    public void onSkipPressed(Fragment currentFragment) {
+        super.onSkipPressed(currentFragment);
+        Intent intent=new Intent(getApplicationContext(),LoginActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
